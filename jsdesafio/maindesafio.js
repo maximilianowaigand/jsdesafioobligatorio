43737c94734bb0f686aa0535252bca4f2693eb6c
Maximiliano Waigand <106201959+maximilianowaigand@users.noreply.github.com>
2022-08-25 23:25:55 -0300

    proyecto final

--- jsdesafio/maindesafio.js
+++ jsdesafio/maindesafio.js
@@ -1,79 +1,14 @@
-/*const productos = [
-    {nombre: "teclado", precio: 1500},
-    {nombre: "mouse", precio: 1000},
-    {nombre: "cpu", precio: 3000},
-    {nombre: "monitor", precio: 10000},
-];
 
-let carrito = []
-
-
- let selection = prompt("desea comprar algun producto si o no")
-
-while(selection != "si" && selection != "no") {
-    alert ("por favor ingrese si o no")
-    selection = promp ("hola desea comprar algo si o no")
-}
-
-if (selection == "si") {
-
-    const nombresyPrecios = productos.map ((el) => el.nombre + " " + el.precio);
-
-    alert ("nuestros productos son " + productos.length +" :\n" + nombresyPrecios.join('\n'));
-
-} else if (selection == "no") {
-    alert ("gracias por venir, hasta pronto")
-}
-
-
-while ( selection != "no") {    
-    let producto = prompt ("agrega productos a tu carrito y ponga esc para terminar");
-    let precio = 0 
-
-    if (producto == "teclado" || producto == "mouse" || producto == "cpu" || producto == "monitor") {
-        switch (producto) {
-            case "teclado":
-                precio = 1500;
-                break;
-            case "mouse":
-                precio = 1000;
-                break;
-            case "cpu":
-                precio = 3000;
-                break;
-            case "monitor":
-                precio = 10000;
-                break;
-            default:
-                 break;
-        }
-
-    let cantidad = parseInt(prompt("cuantas unidades quiere llevar"));
-
-    carrito.push({producto, cantidad, precio})
-    console.log(carrito)
-    } else {
-        alert("no ingreso un producto de la lista o ingreso esc para terminar")
-    }
-
-
- if (producto == "esc") {
-    alert ("gracias por su compra.a continuacion le indicaremos sus productos")
-    carrito.forEach((carritoFinal) => {
-       alert ("usted compro:\n" + carritoFinal.producto + " " + carritoFinal.cantidad + "=" + (carritoFinal.cantidad * carritoFinal.precio) )
-       alert ("el total a pagar por producto:\n" + (carritoFinal.cantidad * carritoFinal.precio) )
-
-
-    })
- break;
- }
-}
-
-const total = carrito.reduce ((ecc, el) => ecc +( el.precio * el.cantidad), 0)
-alert ("el total a pagar es: " + total)*/
 let tarjetas = document.getElementById('tarjetas');
 
-console.log(tarjetas) 
+fetch('./jsdesafio/data.json')
+   .then((Response) => Response.json())
+   .then((data) => { setTimeout (()=> console.log(data), 100)
+    
+});
+
+    
+   
 
 let productos = [
     {
@@ -96,19 +31,22 @@ let productos = [
         img: "imagenes/cpu.jpg" 
     },
     {
-        id:4,
+        id: 4,
         producto: "monitor",
         precio: 10000,        
         img: "imagenes/monitor.jpg"
     },
     ];
+    
+    let cantidad = JSON.parse(localStorage.getItem("data")) || []
 
     
     let generarcompra = () => {
         return (tarjetas.innerHTML = productos.map((e)=>{
-            let {producto, precio, id, img } = e
-            return (tarjetas.innerHTML= `
-            <div id=producto-id-${id} class="card" style="width: 18rem;">
+            let  { id, producto, precio, img } = e;
+            let search = cantidad.find ((e) => e.id === id) || [];
+            return `
+            <div id=producto-id-${id} class="item" style="width: 18rem;">
       <img src="${img}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${producto}</h5>
@@ -118,7 +56,9 @@ let productos = [
         <p class="card-text">$ ${precio}</p>
         <div class"bottons">
         <i onclick="cantidadSuma(${id})" class="bi bi-dash-lg">+</i>
-        <div id=${id} class="quantity">0</div>
+        <div id=${id} class="quantity">${
+            search.item === undefined ? 0 : search.item
+          }</div>
         <i onclick="cantidadResta(${id})" class="bi bi-dash-lg">-</i>
         </div>
         </div>
@@ -126,21 +66,25 @@ let productos = [
           <input class="cantidad" type="text">
         </div>
       </div>
-    </div>`
-        )}).join(" "))
-    }
+    </div>`;
+        })
+        .join(" "));
+    };
 
     generarcompra ();
 
-    let cantidad =[];
+
+
+    
+    
 
     let cantidadSuma = (id) => {
         let selectedItem = id;
-        let search = cantidad.find((e) => e.id === selectedItem.id);
+        let search = cantidad.find((e) => e.id === id);
 
         if (search === undefined) {
             cantidad.push({
-                id: selectedItem.id,
+                id: id,
                 item: 1,
 
             });
@@ -148,26 +92,41 @@ let productos = [
 
         } else {
             search.item += 1;
-        }
+        };
+        Swal.fire('Any fool can use a computer');
+        cantidadTotal(selectedItem);
+        cantidad = cantidad.filter ((e)=> e.item !== 0);
+      // console.log(cantidad);   
+       
+
+       localStorage.setItem("data", JSON.stringify(cantidad));
         
-        console.log(cantidad);
-        cantidadTotal(selectedItem.id)
     };
     let cantidadResta = (id) => {
         let selectedItem = id;
-        let search = cantidad.find((e) => e.id === selectedItem.id);
+        let search = cantidad.find((e) => e.id === id);
 
-        if (search.item === 0) return
+        if (search.item === 0) return;
         else {
             search.item -= 1;
-        }
+        };
         
-        console.log(cantidad);
-        cantidadTotal(selectedItem.id)
+       // console.log(cantidad);
+        cantidadTotal();
 
-    }
+    };
     let cantidadTotal = (id) => {
-        console.log(id)
+        let search = cantidad.find((e)=> e.id === id);
+        //console.log(search.item);
+       document.getElementById(id).innerHTML = search.item;
+       calcular();
+    }; 
+
+    let calcular = () => {
+        let carritoCantidad = document.getElementById("carritoCantidad");
+        carritoCantidad.innerHTML =cantidad.map((e)=>e.item).reduce((e,y)=> e + y, 0);
+
+      
     }
 
 
